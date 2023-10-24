4d66aa551f1e4f32dd197a74d5cc3a6d03f4fc76
Pavlechko <pavlechko.serhii@gmail.com>
2023-10-24 17:10:39 +0200

    Added data context and refactored Home page

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,40 +1,29 @@
-import { FC, useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import Button from "@mui/material/Button";
+import FollowTheSignsIcon from '@mui/icons-material/FollowTheSigns';
 
-import WeatherPage from "./Weather.page";
-import { getData, } from "../services/api.service";
 import { UserContext } from "../context/UserContext";
-import { initialData } from "../utils/initialData";
+import { DataContext } from "../context/DataContext";
+import WeatherPage from "./Weather.page";
 import QuoteCard from "../features/quote/components/quote-card.component";
 import Greeting from "../features/greeting/components/greeting.component";
 
 import "./Home.style.css";
 
+
 const HomePage = () => {
   const { user } = useContext(UserContext)
-  const [data, setData] = useState(initialData)
-
+  const { data } = useContext(DataContext)
+  
+  const clickHandler = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  }
   console.log('Home page is rendering!');
-
-  useEffect(() => {
-    console.log("USE EFFECT Home page!")
-    if (user.loggedIn) {
-      
-      getData()
-        .then(data => {
-          if (data) {
-            console.log(data.data)
-            setData(data.data)
-          } else {
-            console.log("from else")
-          }
-        })
-    }
-  }, [user.loggedIn])
 
   return(
     <div className="home" style={{position: "relative", backgroundImage: `url("${data.Backgroung.image}")`}}>
-      <Link to="/signin">Login</Link>
+      <Button variant="contained" size="large" sx={{backgroundColor: "transparent", fontSize: 18}} endIcon={<FollowTheSignsIcon />} onClick={clickHandler}>SignOut</Button>
       <Greeting name={user.name} />
       <WeatherPage weatherData={data.Weather} />
       <QuoteCard quoteData={data.Quote} />
